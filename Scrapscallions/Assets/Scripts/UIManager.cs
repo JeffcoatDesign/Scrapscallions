0f5c0e8a2211895d770377c05d0874f545ca8b75
LRapier <98057284+LRapier@users.noreply.github.com>
2025-01-15 16:39:44 -0700

    Options Menu and starting functionality

--- Scrapscallions/Assets/Scripts/UIManager.cs
+++ Scrapscallions/Assets/Scripts/UIManager.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     public Texture2D cursor;
+
+    [Header("Menus")]
+    [SerializeField]
+    private GameObject mainMenu, optionsMenu;
     void Start()
     {
         Cursor.SetCursor(cursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    void CloseOptions()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
     }
 }
